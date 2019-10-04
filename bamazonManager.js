0725c03e628d97c0bed5dbc966a58ff0991d333c
Greg D <gregorydesmarais@gmail.com>
2019-10-04 18:55:16 -0400

    Completed addNewProduct function. Tidyed query code for legibility. Added validation functions for input fields. Added additional formatting and information

--- bamazonManager.js
+++ bamazonManager.js
@@ -78,40 +78,117 @@ function viewLowInventory() {
                 console.log("\n\nThere are currently no items that are low in inventory\n\n");
                 return showMenu();
             }
+            console.log("The following items have less than 5 remaining in stock")
             displayData(res);
+            console.log("\n\n");
             showMenu();
         });
 }
 
 function addToInventory() {
     var itemIDs = [];
-    var stock = "";
     connection.query("SELECT * from products",
         function(err, res) {
             if (err) throw err;
             displayData(res);
+            for (x in res) {
+                itemIDs.push(res[x].item_id);
+            }
             inquire.prompt([{
                 message: "Please enter the ID of the item you would like to add stock to:",
                 name: "selectedItem",
                 validate: function(input) {
                     if (!itemIDs.includes(parseInt(input))) {
-                        console.log("\n" + stock);
-                        console.log("\nPlease enter a valid ID\n");
-                        return false;
+                        console.clear();
+                        displayData(res);
+                        return "Please enter a valid ID"
                     } else return true;
                 }
             }, {
                 message: "How many would you like to add?",
-                name: "requestedQty"
+                name: "requestedQty",
+                validate: validateInteger
             }]).then(function(ans) {
                 var selectedItem = res[parseInt(ans.selectedItem) - 1];
                 var newQuantity = selectedItem.stock_quantity + parseInt(ans.requestedQty);
-                connection.query(`UPDATE products SET stock_quantity = "${newQuantity}" WHERE item_id=${parseInt(ans.selectedItem)}`, function(err, res) {
-                    if (err) throw err;
-                    console.clear();
-                    console.log(`Stock for ${selectedItem.product_name} has been updated from ${selectedItem.stock_quantity} to ${newQuantity}!`);
-                    return showMenu();
-                })
+                connection.query(`UPDATE products SET ? WHERE ?`, [{
+                            stock_quantity: newQuantity
+                        },
+                        {
+                            item_id: parseInt(ans.selectedItem)
+                        }
+                    ],
+                    function(err, res) {
+                        if (err) throw err;
+                        console.clear();
+                        console.log(`Stock for ${selectedItem.product_name} has been updated from ${selectedItem.stock_quantity} to ${newQuantity}!`);
+                        return showMenu();
+                    })
             })
         });
+}
+
+function addNewProduct() {
+    inquire.prompt([{
+            message: "Enter the product name:",
+            name: "product_name",
+            validate: checkText
+        },
+        {
+            message: "Enter the department for the product:",
+            name: "department_name",
+            validate: checkText
+        },
+        {
+            message: "Enter the price for the item:",
+            name: "price",
+            validate: validateNumeric
+        },
+        {
+            message: "Enter the inital stock for this product:",
+            name: "stock_quantity",
+            validate: validateInteger
+        }
+    ]).then(function(ans) {
+        connection.query("INSERT INTO products SET ?", {
+            product_name: ans.product_name,
+            department_name: ans.department_name,
+            price: ans.price,
+            stock_quantity: ans.stock_quantity
+        }, function(err, res) {
+            if (err) throw err;
+            console.clear();
+            console.log(`Added ${ans.product_name} to the database!`);
+            showMenu();
+        });
+    })
+}
+
+function checkText(data) {
+    if (data.length <= 0)
+        return "This field cannot be left blank."
+    else return true;
+}
+
+function validateInteger(value) {
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
+
+    if (integer && (sign === 1)) {
+        return true;
+    } else {
+        return 'Please enter a whole non-zero number.';
+    }
+}
+
+function validateNumeric(value) {
+    // Value must be a positive number
+    var number = (typeof parseFloat(value)) === 'number';
+    var positive = parseFloat(value) > 0;
+
+    if (number && positive) {
+        return true;
+    } else {
+        return 'Please enter a positive number for the price.'
+    }
 }
