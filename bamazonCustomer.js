ad64bb4f5269897749d9ae5fef78f2e331d5a144
Greg D <gregorydesmarais@gmail.com>
2019-10-04 14:37:19 -0400

    Completed customer page.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,18 +12,50 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Showing stock");
     showStock();
 });
 
 function showStock() {
-    console.log("Retrieving stock")
-    connection.query("SELECT * from products",
+    var itemIDs = [];
+    var stock = "";
+    connection.query("SELECT * from products WHERE stock_quantity > 0",
         function(err, res) {
             if (err) throw err;
             for (x in res) {
-                console.log(`${res[x].product_name} || ${res[x].department_name} || ${res[x].price} || ${res[x].stock_quantity}`)
+                itemIDs.push(res[x].item_id);
+                stock += `Item ID: ${res[x].item_id} || Name: ${res[x].product_name} || Price: $${res[x].price.toFixed(2)}\n`;
             }
+            console.log(stock);
+            inquire.prompt([{
+                message: "Please enter the ID of the item you would like to buy:",
+                name: "selectedItem",
+                validate: function(input) {
+                    if (!itemIDs.includes(parseInt(input))) {
+                        console.log("\n" + stock);
+                        console.log("\nPlease enter a valid ID\n");
+                        return false;
+                    } else return true;
+                }
+            }, {
+                message: "How many would you like to purchase?",
+                name: "requestedQty"
+            }]).then(function(ans) {
+                connection.query(`SELECT * from products WHERE item_id="${ans.selectedItem}"`, function(err, res) {
+                    if (err) throw err;
+                    if (res[0].stock_quantity >= parseInt(ans.requestedQty)) {
+                        var price = res[0].price;
+                        connection.query(`UPDATE products SET stock_quantity = "${res[0].stock_quantity - parseInt(ans.requestedQty)}" WHERE item_id=${parseInt(ans.selectedItem)}`, function(err, res) {
+                            if (err) throw err;
+                            console.log(`
+Order placed!
+Your total cost is: $${price * parseInt(ans.requestedQty)}`);
+                        })
+                        connection.end();
+                    } else {
+                        console.log("Insufficient Quantity");
+                        showStock();
+                    }
+                })
+            })
         });
-    connection.end();
 }
