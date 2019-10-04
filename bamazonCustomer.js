d7a35d73407ad374f464baae86726a06d1074a3e
Greg D <gregorydesmarais@gmail.com>
2019-10-04 13:45:00 -0400

    Successfully connected to and retreive data from database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,29 @@
 var mysql = require("mysql");
 var inquire = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+
+    user: 'root',
+    password: '',
+    database: 'bamazon'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Showing stock");
+    showStock();
+});
+
+function showStock() {
+    console.log("Retrieving stock")
+    connection.query("SELECT * from products",
+        function(err, res) {
+            if (err) throw err;
+            for (x in res) {
+                console.log(`${res[x].product_name} || ${res[x].department_name} || ${res[x].price} || ${res[x].stock_quantity}`)
+            }
+        });
+    connection.end();
+}
