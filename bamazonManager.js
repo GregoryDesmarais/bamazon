08b783dd3ecec67d430ce6db10c59f7a5cd22151
Greg D <gregorydesmarais@gmail.com>
2019-10-04 18:10:26 -0400

    completed addToInventory function.  Added displayData function to reduce redundant code.

--- bamazonManager.js
+++ bamazonManager.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
     password: '',
     database: 'bamazon'
 });
-
+console.clear();
 connection.connect(function(err) {
     if (err) throw err;
     showMenu();
@@ -30,31 +30,41 @@ function showMenu() {
     }]).then(function(ans) {
         switch (ans.menuSelect) {
             case "View Products for Sale":
+                console.clear();
                 viewProducts();
                 break;
             case "View Low Inventory":
+                console.clear();
                 viewLowInventory();
                 break;
             case "Add to Inventory":
+                console.clear();
                 addToInventory();
                 break;
             case "Add New Product":
+                console.clear();
                 addNewProduct();
                 break;
             case "Exit":
+                console.clear();
                 connection.end();
                 break;
         }
     })
 }
 
+function displayData(res) {
+    for (x in res) {
+        console.log(`Item ${res[x].item_id}: || Name: ${res[x].product_name} || Department: ${res[x].department_name} || Price: $${res[x].price.toFixed(2)} || Quantity: ${res[x].stock_quantity}`);
+    }
+}
+
+
 function viewProducts() {
     connection.query("SELECT * from products",
         function(err, res) {
             if (err) throw err;
-            for (x in res) {
-                console.log(`Item ${res[x].item_id}: || Name: ${res[x].product_name} || Department: ${res[x].department_name} || Price: $${res[x].price.toFixed(2)} || Quantity: ${res[x].stock_quantity}|`);
-            }
+            displayData(res);
             showMenu();
         });
 }
@@ -63,13 +73,45 @@ function viewLowInventory() {
     connection.query("SELECT * from products WHERE stock_quantity < 5",
         function(err, res) {
             if (err) throw err;
-            if (res.length < 2) {
+            if (res.length < 1) {
+                console.clear();
                 console.log("\n\nThere are currently no items that are low in inventory\n\n");
-                showMenu();
+                return showMenu();
             }
-            for (x in res) {
-                console.log(`Item ${res[x].item_id}: || Name: ${res[x].product_name} || Department: ${res[x].department_name} || Quantity: ${res[x].stock_quantity}|`);
-            }
+            displayData(res);
+            showMenu();
         });
-    showMenu();
+}
+
+function addToInventory() {
+    var itemIDs = [];
+    var stock = "";
+    connection.query("SELECT * from products",
+        function(err, res) {
+            if (err) throw err;
+            displayData(res);
+            inquire.prompt([{
+                message: "Please enter the ID of the item you would like to add stock to:",
+                name: "selectedItem",
+                validate: function(input) {
+                    if (!itemIDs.includes(parseInt(input))) {
+                        console.log("\n" + stock);
+                        console.log("\nPlease enter a valid ID\n");
+                        return false;
+                    } else return true;
+                }
+            }, {
+                message: "How many would you like to add?",
+                name: "requestedQty"
+            }]).then(function(ans) {
+                var selectedItem = res[parseInt(ans.selectedItem) - 1];
+                var newQuantity = selectedItem.stock_quantity + parseInt(ans.requestedQty);
+                connection.query(`UPDATE products SET stock_quantity = "${newQuantity}" WHERE item_id=${parseInt(ans.selectedItem)}`, function(err, res) {
+                    if (err) throw err;
+                    console.clear();
+                    console.log(`Stock for ${selectedItem.product_name} has been updated from ${selectedItem.stock_quantity} to ${newQuantity}!`);
+                    return showMenu();
+                })
+            })
+        });
 }
